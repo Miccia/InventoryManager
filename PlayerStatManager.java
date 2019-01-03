c48a611fd42e1b474916006ab64ed5fe4fa9acaf
Miccia <miccia@miccialabs.com>
2019-01-03 23:48:22 +0100

    javass

---
+++ PlayerStatManager.java
@@ -0,0 +1,23 @@
+class PlayerStatManager{
+	
+	private float currentHealth;
+	private float currentStamina;
+	private float currentHunger;
+	private float currentThirst;
+	private float currentTemperature;
+	private float baseMeleeDamage;
+	private float baseSpeed;
+	private float speedMultiplier;
+
+	private float topHealth;
+	private float topStamina;
+	private float topHunger;
+	private float topThirst;
+	private float topTemperature;
+
+	private PlayerState 
+
+
+
+
+}
