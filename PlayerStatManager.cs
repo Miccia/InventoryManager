16c094b5d794fd7c7593a35303032ce13ff61c5f
Miccia <miccia@miccialabs.com>
2019-01-03 23:02:22 +0100

    First Commit!

---
+++ PlayerStatManager.cs
@@ -0,0 +1,46 @@
+
+class PlayerStatManager{
+	
+	//needed to 'link' the animator
+	enum PlayerState{ idle, running, attack, attack_running, hit, dead, poisoned, bloody, crippled }
+	
+	public const int MAX_LEVEL = 180;
+
+	private float currentExperience;
+	private float newLevelExp;
+	private int currentLevel;
+	private int newLevels;
+
+	private float topHealth;
+	private float topStamina;
+	private float topHunger;
+	private float topThirst;
+	private float topTemperature;
+
+	private float currentHealth;
+	private float currentStamina;
+	private float currentHunger;
+	private float currentThirst;
+	private float currentTemperature;
+
+	private float baseMeleeDamage;
+	private float meleeDamageMultiplier;
+	private float baseSpeed;
+	private float speedMultiplier;
+
+	
+	private PlayerState     currentState;
+	private StatsEffects[]  activeEffects;
+
+//TO DO 
+	public PlayerState getCurrentState(){return currentState;}
+	
+	public setState(PlayerState ps){currentState=ps;}
+	
+
+	public addStatsEffects(){}
+
+
+
+
+}
---
+++ StatsEffects.cs
@@ -0,0 +1,12 @@
+class StatsEffects{
+
+	public int id;
+	public bool isImmediate;   
+	public float duration;
+	public float spawnTime;
+
+	private PlayerStatManager effect;
+
+	//TO DO methods
+
+}
